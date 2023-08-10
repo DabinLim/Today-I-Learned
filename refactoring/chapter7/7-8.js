9eb6d4eb9c7432a793adaf8aff8eba397c6ae32f
DabinLim <dabinim09@gmail.com>
2023-08-10 14:22:55 +0900

    refactoring 7-8

--- refactoring/chapter7/7-8.js
+++ refactoring/chapter7/7-8.js
@@ -1,1 +1,74 @@
-// 중개자 제거하기
+// 중개자 제거하기 (위임 숨기기 반대)
+
+(function after1() {
+  class Person {
+    constructor(name) {
+      this._name = name;
+    }
+    get name() {
+      return this._name;
+    }
+    get department() {
+      return this._department;
+    }
+    set department(arg) {
+      this._department = arg;
+    }
+    get manager() {
+      return this._department.manager;
+    }
+  }
+
+  class Department {
+    get chargeCode() {
+      return this._chargeCode;
+    }
+    set chargeCode(arg) {
+      this._chargeCode = arg;
+    }
+    get manager() {
+      return this._manager;
+    }
+    set manager(arg) {
+      this._manager = arg;
+    }
+  }
+
+  const aPerson = new Person("다빈잉");
+  manager = aPerson.manager;
+})();
+
+(function after1() {
+  class Person {
+    constructor(name) {
+      this._name = name;
+    }
+    get name() {
+      return this._name;
+    }
+    get department() {
+      return this._department;
+    }
+    set department(arg) {
+      this._department = arg;
+    }
+  }
+
+  class Department {
+    get chargeCode() {
+      return this._chargeCode;
+    }
+    set chargeCode(arg) {
+      this._chargeCode = arg;
+    }
+    get manager() {
+      return this._manager;
+    }
+    set manager(arg) {
+      this._manager = arg;
+    }
+  }
+
+  const aPerson = new Person("다빈잉");
+  manager = aPerson.department.manager;
+})();
